5e783a9e305fa9a3462295402084a733101f1ba5
maxym <maksym.iurchenko@gmail.com>
2019-05-31 15:02:10 -0700

    ...

--- src/homeTest/Main.java
+++ src/homeTest/Main.java
@@ -1,6 +1,8 @@
 package homeTest;
 
+import java.sql.SQLOutput;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,7 +21,6 @@ public class Main {
         for (int i = 0; i < expectedElements.length; i++) {
             expectedArrayList.add(expectedElements[i]);
         }
-
 
 
         Iterator<Integer> iterator = null;
@@ -45,17 +46,20 @@ public class Main {
             System.out.println("test pass");
         }
 
-        Integer[] testElement  = {1,2,3,4,5,6,7,8};
+
+
+        //test linked list
+        Integer[] testElement = {1, 2, 3, 4, 5, 6, 7, 8};
         //first array list
         SimpleArrayList<Integer> firstSimpleArrayList = new SimpleArrayList<>();
-        for (int i = 0; i <testElement.length ; i++) {
+        for (int i = 0; i < testElement.length; i++) {
             firstSimpleArrayList.add(testElement[i]);
         }
 
         //second array list
         SimpleArrayList<Integer> secondSimpleArrayList = new SimpleArrayList<>();
 
-        for (int i = 0; i <testElement.length-2; i++) {
+        for (int i = 0; i < testElement.length - 2; i++) {
             secondSimpleArrayList.add(testElement[i]);
         }
         System.out.println(firstSimpleArrayList.hashCode());
@@ -65,6 +69,32 @@ public class Main {
             System.out.println("test passed 2");
         }
 
+
+        //check linked list
+
+        Integer[] testElement2  = {1,2,3,4,5,6,7,8};
+        //first array list
+        SimpleLinkedList<Integer> firstSimpleLinkedList = new SimpleLinkedList<>();
+        for (int i = 0; i <testElement2.length ; i++) {
+            firstSimpleLinkedList.add(testElement2[i]);
+        }
+
+        //second array list
+        SimpleLinkedList<Integer> secondSimpleLinkedList = new SimpleLinkedList<>();
+
+        for (int i = 0; i <testElement2.length ; i++) {
+            secondSimpleLinkedList.add(testElement2[i]);
+        }
+
+        System.out.println(firstSimpleLinkedList.toString());
+        System.out.println(secondSimpleLinkedList.toString());
+
+        //check
+        if (!firstSimpleLinkedList.equals(secondSimpleLinkedList))
+            throw new AssertionError("Two simpleLinkedList with identical type and with identical elements should be equals");
+
+
+        System.out.print("OK");
     }
 
 }
